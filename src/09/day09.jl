282fe338669885c8f82714e901f5bcc54817cc04
Jarkko Mourujarvi <jarkkom@unity3d.com>
2022-12-21 11:58:58 +0200

    WIP

--- src/09/day09.jl
+++ src/09/day09.jl
@@ -83,6 +83,82 @@ function parse_moves(lines)
     return visited
 end
 
+function parse_rope_moves(lines, rope_length)
+    visited = Set()
+
+    rope = Array{Tuple{Int64, Int64}}(undef, rope_length)
+    fill!(rope, (0, 0))
+
+    push!(visited, rope[rope_length])
+
+    for l in lines
+        (dir, count) = split(l, " ")
+        dpos = (0, 0)
+        if dir == "R"
+            dpos = (1, 0)
+        elseif dir == "U"
+            dpos = (0, -1)
+        elseif dir == "D"
+            dpos = (0, 1)
+        elseif dir == "L"
+            dpos = (-1, 0)
+        end 
+
+        c = parse(Int64, count)
+        for _ in 1:c
+            println(rope)
+
+            # move head
+            rope[1] = (rope[1][1] + dpos[1], rope[1][2] + dpos[2])
+
+            # move 
+            for section in 2:rope_length
+                dx = abs(rope[section - 1][1] - rope[section][1])
+                dy = abs(rope[section - 1][2] - rope[section][2])
+
+                if (dx == 0 || dy == 0) && (dx > 1 || dy > 1)
+                    # FIXME
+                    
+                    rope[section] = (rope[section][1] + dpos[1], rope[section][2] + dpos[2])
+                    if section == rope_length
+                        push!(visited, rope[section])
+                    end
+                    continue
+                end
+
+                if (dx + dy) < 2 || (dx == dy)
+                    if section == rope_length
+                        push!(visited, rope[section])
+                    end
+                    continue
+                end
+
+                if (dx > 1 && dy == 1) || (dy > 1 && dx == 1)
+                    if rope[section][1] < rope[section - 1][1]; rope[section] = (rope[section][1] + 1, rope[section][2]); end
+                    if rope[section][1] > rope[section - 1][1]; rope[section] = (rope[section][1] - 1, rope[section][2]); end
+
+                    if rope[section][2] < rope[section - 1][2]; rope[section] = (rope[section][1], rope[section][2] + 1); end
+                    if rope[section][2] > rope[section - 1][2]; rope[section] = (rope[section][1], rope[section][2] - 1); end
+
+                    if section == rope_length
+                        push!(visited, rope[section])
+                    end
+                    continue
+                end
+
+                println("oops! dx = $(dx) dy = $(dy)")
+                return visited
+            end
+        end
+    end
+
+    return visited
+end
+
+
 @assert(length(parse_moves(sample)) == 13)
 println("part 1 answer = $(length(parse_moves(input)))")
 @assert(length(parse_moves(input)) == 6030)
+
+@assert(length(parse_rope_moves(sample, 10)) == 1)
+println("part 2 answer = $(length(parse_rope_moves(input, 10)))")
