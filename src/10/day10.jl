d4dd6bc613ff18f87b4bc92ee8e82e7922a8651d
Jarkko Mourujarvi <jarkkom@unity3d.com>
2023-01-02 14:59:06 +0200

    Day 10 part 2

--- src/10/day10.jl
+++ src/10/day10.jl
@@ -46,6 +46,53 @@ function sum_strengths(input)
     return strength_sum
 end
 
+function draw_pixel(x, cycle)
+    beam_pos = cycle % 40
+
+    if beam_pos >= x - 1 && beam_pos < x + 2
+        print("#")
+    else
+        print(".")
+    end
+
+    if beam_pos == 39
+        println()
+    end
+end
+
+function draw_sprite(input)
+    cycle = 0
+    x = 1
+    for l in input
+        if l == "noop"
+            draw_pixel(x, cycle)
+            cycle += 1
+            continue
+        end
+        if startswith(l, "addx ")
+            param = parse(Int64, split(l)[2])
+
+            draw_pixel(x, cycle)
+            cycle += 1
+
+            draw_pixel(x, cycle)
+            cycle += 1
+
+            x += param
+        end
+    end
+
+    while cycle < 240
+        draw_pixel(x, cycle)
+        cycle += 1
+    end
+end
+
+
 @assert(sum_strengths(sample) == 13140)
 println("part 1 answer = $(sum_strengths(input))")
-@assert(length(parse_moves(input)) == 15880)
+@assert(sum_strengths(input) == 15880)
+
+draw_sprite(sample)
+println()
+draw_sprite(input)
